eecb2601fea1c1e4a3d6f3288c8a119eab6214a4
awujo <71509142+awujo@users.noreply.github.com>
2022-02-16 13:21:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <html>
 <head></head>
 <body>
-<img src="https://www.freepnglogos.com/uploads/hacker-png/hacker-png-png-foto-24.png" width="200" alt="hacker png png foto" />
+<img src="https://commons.wikimedia.org/wiki/File:Hackattackbtcimg.jpg" width="200" alt="hacker png png foto" />
     <p>lol</p>
 </body>
 </html>
