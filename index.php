513ee3fe89351de512fc700f0fc53538c9ed044b
awujo <71509142+awujo@users.noreply.github.com>
2022-02-16 13:38:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,12 @@
 ?>
 
 <html>
-<head></head>
-<body>
-<img src="https://i.ibb.co/YtBsrhM/Screenshot-20220216-112933-1.jpg" alt="Screenshot-20220216-112933-1" border="0">
+<head><style>
+    .body{
+    text-align:center;}
+    </style>
+    </head>
+<body><div class="body">
+<img src="https://i.ibb.co/YtBsrhM/Screenshot-20220216-112933-1.jpg" alt="Screenshot-20220216-112933-1" border="0"></div>
 </body>
 </html>
