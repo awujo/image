b75f731788066b1a0e7eb9081bcc0fb106490f39
awujo <71509142+awujo@users.noreply.github.com>
2022-02-16 13:36:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
 <html>
 <head></head>
 <body>
-<img src="<img src="https://i.ibb.co/YtBsrhM/Screenshot-20220216-112933-1.jpg" alt="Wallet QR Code" border="0">" width="200" alt="hacker png png foto" />
+<img src="https://i.ibb.co/YtBsrhM/Screenshot-20220216-112933-1.jpg" alt="Screenshot-20220216-112933-1" border="0">
 </body>
 </html>
